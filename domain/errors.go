98d50c035f216c8d2186cf9716489ffab0ab57fe
Kalys Nurmanbetov <knu@Kalyss-MacBook-Pro.fritz.box>
2026-09-19 13:20:10 +0200

    Remove confusing comments

--- domain/errors.go
+++ domain/errors.go
@@ -5,15 +5,12 @@ import (
 	"fmt"
 )
 
-// Invariants Errors
 var ErrTaskAlreadyCompleted = errors.New("task is already completed")
 var ErrTaskAlreadyUncompleted = errors.New("task is already uncompleted")
 var ErrChangeTitleOfAlreadyCompleted = fmt.Errorf("title cannot be change %w", ErrTaskAlreadyCompleted)
 
-// Fabric Errors
 var ErrInvalidTaskId = errors.New("invalid task id")
 var ErrEmptyTitle = errors.New("title must be not empty")
 var ErrTooLongTitle = errors.New("title should contain max 200 characters")
 
-// Repository Errors
 var ErrTaskNotFound = errors.New("task is not found")
